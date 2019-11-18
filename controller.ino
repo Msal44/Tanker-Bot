eff833115a7b936dfaa92c758fa8b3c059a49409
Melvyn S <41174650+Msal44@users.noreply.github.com>
2019-11-18 14:21:44 -0600

    Create controller.ino

---
+++ controller.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+Servo myservo; // create servo object to control a servo
+/////////////////////////////////////////////////////////////
+SoftwareSerial mySerial(11,9); //sending and recieveing      
+/////////////////////////////////////////////////////////////
+  int valX;
+  int valY;
+  int valJB;
+  int valBB;
+  
+  int joypinY = 6;
+  int joypinX = 7;
+  int joybuttonpin = 12;
+  int buttonpin = 13;
+  
+void setup() {
+  //myservo.attach(6); // attatches the servo on pin 6 to the servo object
+
+  pinMode(joybuttonpin, INPUT_PULLUP); //initializing button as input, set the internal pull up resistor, unpressed is HIGH
+  pinMode(buttonpin, INPUT);
+  mySerial.begin(57600);
+  Serial.begin(57600);
+
+}
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+  valX = analogRead(joypinX); // reads the value of the potentiometer (value between 0 and 1023)
+  valY = analogRead(joypinY);
+  valJB = digitalRead(joybuttonpin);
+  valBB = digitalRead(buttonpin);
+
+Serial.println("bluh");
+
+  valX = map(valX, 0, 1023, 0, 255); // scale it to use it with the servo (value between 0 and 180)
+  valY = map(valY, 0, 1023, 0, 255);
+  mySerial.print('A');  // this signifies the start of the serial code
+  mySerial.print(char(valX));
+  mySerial.print(char(valY));
+  mySerial.print(char(valJB));
+  mySerial.print(char(valBB));
+  mySerial.print('Z'); // this signifies the end of the serial code
+  delay(15);
+  Serial.println("end of function");
+ 
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////// 
+  
+ }
